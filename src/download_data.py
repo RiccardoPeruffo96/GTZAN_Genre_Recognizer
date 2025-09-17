5f31d14b1cc4d7647f1c64cadd77cb4bc0550215
Riccardo Peruffo <rperuffo@deloitte.it>
2025-09-17 12:09:13 +0200

    Add check dataset exists v03

---
+++ src/download_data.py
@@ -0,0 +1,12 @@
+######################
+### NOT TESTED YET ###
+######################
+
+# Script copied from Kaggle documentation 
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("achgls/gtzan-music-genre")
+
+print("Path to dataset files:", path)
